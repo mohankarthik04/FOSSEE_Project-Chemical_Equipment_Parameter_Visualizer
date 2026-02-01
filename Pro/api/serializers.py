c148007c44a6004525c58da53fcef5e7f1885fb8
Mohan Karthik Saravanan <mohankarthiksaravanan@gmail.com>
2026-02-01 11:40:00 +0530

    Delete Pro directory

--- Pro/api/serializers.py
+++
@@ -1,8 +0,0 @@
-# Pro/api/serializers.py
-from rest_framework import serializers
-from .models import EquipmentData
-
-class EquipmentDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EquipmentData
-        fields = '__all__'
